ce63b37a1ea12fddc2b694b49a3fa92b8103bc62
datkidnik <s3379952@student.rmit.edu.au>
2017-04-14 23:27:39 +1000

    Update KDTreeNN.java

--- KDTreeNN.java
+++ KDTreeNN.java
@@ -159,7 +159,7 @@ public class KDTreeNN implements NearestNeigh{
         if(median > 0){
             // flip() inverts the boolean value (effectively changing the dimension we split on next) 
             leftTree = sortedPoints;
-            leftTree = leftTree.subList(median, sizeOfTree);
+            leftTree = leftTree.subList(0, median);
             leftChild = buildTree(leftTree, flip(bXDim), currParent); 
         } 
             
@@ -167,7 +167,7 @@ public class KDTreeNN implements NearestNeigh{
         if(median < sizeOfTree){
             // flip() inverts the boolean value (effectively changing the dimension we split on next) 
             rightTree = sortedPoints;
-            rightTree = rightTree.subList(0, median);
+            rightTree = rightTree.subList(median, sizeOfTree);
             rightChild = buildTree(rightTree, flip(bXDim), currParent); 
         }       
         currParent.setLeftChild(leftChild); 
